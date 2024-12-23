dc1f953b7a3e1c42f864e4e2fb94fe88127faa3e
Lev Bazdyriev <c.coder07@gmail.com>
2024-12-23 16:57:10 +0200

    Upd!

--- src/main.rs
+++ src/main.rs
@@ -1,143 +1,129 @@
-use clap::{Arg, Parser};
-use std::{thread, time::Duration, io::Read, str};
+use clap::Parser;
+use crossbeam_channel::{bounded, select, tick, unbounded, Receiver};
+use crossterm::{
+    cursor::*,
+    event,
+    event::poll,
+    event::{Event, KeyCode, KeyEvent},
+    execute,
+    style::*,
+    terminal::{disable_raw_mode, enable_raw_mode, size, Clear, ClearType},
+    ExecutableCommand, QueueableCommand, Result,
+};
 
-const CRSF_SYNC: u8 = 0xC8;
+use std::io::stdout;
+use std::thread;
+use std::time::Duration;
+use msp::MSPMsg;
+use msp::States;
 
-#[derive(Debug)]
-enum PacketTypes {
-    GPS = 0x02,
-    Vario = 0x07,
-    BatterySensor = 0x08,
-    BaroAlt = 0x09,
-    Heartbeat = 0x0B,
-    VideoTransmitter = 0x0F,
-    LinkStatistics = 0x14,
-    RcChannelsPacked = 0x16,
-    Attitude = 0x1E,
-    FlightMode = 0x21,
-    DeviceInfo = 0x29,
-    ConfigRead = 0x2C,
-    ConfigWrite = 0x2D,
-    RadioID = 0x3A,
-}
+mod msp;
+
 
 #[derive(Parser, Debug)]
 #[command(name = "CRSF Packet Parser")]
 #[command(about = "A tool to parse CRSF packets", long_about = None)]
 struct Args {
-    #[arg(short = 'P', long, default_value = "/dev/ttyAMA0")]
+    #[arg(short = 'P', long, default_value = "/dev/ttyACM0")]
     port: String,
-    #[arg(short = 'b', long, default_value_t = 420000)]
+    #[arg(short = 'b', long, default_value_t = 115200)]
     baud: u32,
 }
 
-fn crc8_dvb_s2(mut crc: u8, a: u8) -> u8 {
-    crc ^= a;
+pub fn get_serial_devices(defdev: &str, testcvt: bool) -> String
+{
+    match serialport::available_ports() {
+        Ok(ports) => {
+            for p in ports {
+                match &p.port_type {
+                    serialport::SerialPortType::UsbPort(pt) => {
+                        if (pt.vid == 0x0483 && pt.pid == 0x5740)
+                            || (pt.vid == 0x0403 && pt.pid == 0x6001)
+                            || (testcvt && (pt.vid == 0x10c4 && pt.pid == 0xea60))
+                        {
+                            return p.port_name.clone();
+                        }
+                    }
+                    _ => {
+                        if std::env::consts::OS == "freebsd" && &p.port_name[0..9] == "/dev/cuaU" {
+                            return p.port_name.clone();
+                        }
+                    }
+                }
+            }
 
-    for _ in 0..8 {
-        if crc & 0x80 != 0 {
-            crc = (crc << 1) ^ 0xD5;
-        } else {
-            crc <<= 1;
-        }
-    }
-
-    return crc & 0xFF
-}
-
-fn crc8_data (data: &[u8]) -> u8 {
-    return data.iter().fold(0, |crc, &byte| crc8_dvb_s2(crc, byte));
-}
-
-fn crsf_validate_frame(frame: &[u8]) -> bool {
-    return crc8_data(&frame[2..frame.len() - 1]) == frame[frame.len() - 1];
-}
-
-fn signed_byte(b: u8) -> i8 {
-    if b >= 128 {
-        return (b as i16 - 256) as i8;
-    } else {
-        return b as i8;
+            return defdev.to_string();
+        },
+        Err(_e) => defdev.to_string()    
     }
 }
 
-fn handle_crsf_packet(ptype: u8, data: &[u8]) {
-    match ptype {
-        x if x == PacketTypes::GPS as u8 => {
-            let lat = i32::from_be_bytes([data[3], data[4], data[5], data[6]]) as f32 / 1e7;
-            let lon = i32::from_be_bytes([data[7], data[8], data[9], data[10]]) as f32 / 1e7;
-            let gspd = i16::from_be_bytes([data[11], data[12]]) as f32 / 36.0;
-            let hdg = i16::from_be_bytes([data[13], data[14]]) as f32 / 100.0;
-            let alt = i16::from_be_bytes([data[15], data[16]]) as i32 - 1000;
-            let sats = data[17];
-            println!(
-                "GPS: Pos={} {} GSpd={:.1}m/s Hdg={:.1} Alt={}m Sats={}",
-                lat, lon, gspd, hdg, alt, sats
-            );
-        }
-        x if x == PacketTypes::Vario as u8 => {
-            let vspd = i16::from_be_bytes([data[3], data[4]]) as f32 / 10.0;
-            println!("VSpd: {:.1}m/s", vspd);
-        }
-        x if x == PacketTypes::Attitude as u8 => {
-            let pitch = i16::from_be_bytes([data[3], data[4]]) as f32 / 10000.0;
-            let roll = i16::from_be_bytes([data[5], data[6]]) as f32 / 10000.0;
-            let yaw = i16::from_be_bytes([data[7], data[8]]) as f32 / 10000.0;
-            println!("Attitude: Pitch={:.2} Roll={:.2} Yaw={:.2} (rad)", pitch, roll, yaw);
-        }
-        x if x == PacketTypes::BaroAlt as u8 => {
-            let alt = i32::from_be_bytes([data[3], data[4], data[5], data[6]]) as f32 / 100.0;
-            println!("Baro Altitude: {:.2}m", alt);
-        }
-        x if x == PacketTypes::LinkStatistics as u8 => {
-            let rssi1 = signed_byte(data[3]);
-            let rssi2 = signed_byte(data[4]);
-            let lq = data[5];
-            let snr = signed_byte(data[6]);
-            println!("RSSI={}/{}dBm LQ={:03}", rssi1, rssi2, lq);
-        }
-        x if x == PacketTypes::BatterySensor as u8 => {
-            let vbat = i16::from_be_bytes([data[3], data[4]]) as f32 / 10.0;
-            let curr = i16::from_be_bytes([data[5], data[6]]) as f32 / 10.0;
-            let mah = (data[7] as u32) << 16 | (data[8] as u32) << 8 | data[9] as u32;
-            let pct = data[10];
-            println!("Battery: {:.2}V {:.1}A {}mAh {}%", vbat, curr, mah, pct);
-        }
-        _ => {
-            println!("Unknown packet type: {:x}", ptype);
+fn wait_for_key (cc: &Receiver<u8>, tot: u64, itm: u32) -> bool
+{
+    let ticks = tick(Duration::from_millis(tot));
+    let mut j = 0;
+    loop {
+        select! {
+            recv(ticks) -> _ => {
+                j += 1;
+                if j == itm {
+                    return true;
+                }
+            }
+            recv(cc) -> res => {
+                if let Ok(x) = res  {
+                    if x == b'Q' { return false}
+                }
+            }
         }
     }
+}
+
+fn outvalue(y: u16, val: &str) -> Result<()>
+{
+    stdout()
+        .execute(MoveTo(10, y))?
+        .execute(SetAttribute(Attribute::Bold))?
+        .execute(Print(val))?
+        .execute(SetAttribute(Attribute::Reset))?
+        .execute(Clear(ClearType::UntilNewLine))?;
+
+    Ok(())
 }
 
 fn main() {
     let args: Args = Args::parse();
-    let mut port = serialport::new(args.port, args.baud)
-        .timeout(Duration::from_secs(2))
+    let defdev= "auto";
+    let _pname: String;
+    let mut buffer  = [0u8; 256];
+    let n = States::Init;
+
+    _pname = get_serial_devices(defdev, true);
+
+    let mut port: Box<dyn serialport::SerialPort> = serialport::new(_pname, args.baud)
+        .timeout(Duration::from_millis(50))
         .open()
-        .expect("Failed to open serial port");
-    let mut input_buffer: Vec<u8> = Vec::new();
-    let mut read_buffer = [0u8; 1024];
+        .expect("Error open port!");
 
     loop {
-        match port.read(&mut read_buffer) {
-            Ok(n) => input_buffer.extend_from_slice(&read_buffer[..n]),
-            Err(_) => thread::sleep(Duration::from_millis(10)),
-        }
+        let payload: [u8; 0] = [];
 
-        while input_buffer.len() > 2 {
-            let expected_len = (input_buffer[1] + 2) as usize;
+        let request = msp::encode_msp(msp::MSG_BOARD_INFO, &payload);
+        port.write_all(&request).expect("Err request send");
 
-            if expected_len > 64 || expected_len < 4 {
-                input_buffer.clear();
-            } else if input_buffer.len() >= expected_len {
-                let single_packet = input_buffer.drain(..expected_len).collect::<Vec<u8>>();
-                
-                if crsf_validate_frame(&single_packet) {
-                    println!("CRC error: {:?}", single_packet);
+        let mut response = [0u8; 1024];
+        match port.read(&mut response) {
+            Ok(bytes_read) => {
+                let board = if response.len() > 8 {
+                    String::from_utf8_lossy(&response[9..])
                 } else {
-                    handle_crsf_packet(single_packet[2], &single_packet);
-                }
+                    String::from_utf8_lossy(&response[0..4])
+                };
+
+                outvalue(10, &board).unwrap();
+                // Тут потрібно розпарсити MSP-відповідь
             }
+            Err(e) => eprintln!("Помилка читання: {}", e),
         }
     }
 }
---
+++ src/msp.rs
@@ -0,0 +1,251 @@
+pub const MSG_IDENT: u16 = 100;
+pub const MSG_NAME: u16 = 10;
+pub const MSG_API_VERSION: u16 = 1;
+pub const MSG_FC_VARIANT: u16 = 2;
+pub const MSG_FC_VERSION: u16 = 3;
+pub const MSG_BOARD_INFO: u16 = 4;
+pub const MSG_BUILD_INFO: u16 = 5;
+pub const MSG_WP_GETINFO: u16 = 20;
+pub const MSG_RAW_GPS: u16 = 106;
+pub const MSG_ANALOG: u16 = 110;
+pub const MSG_DEBUGMSG: u16 = 253;
+pub const MSG_STATUS_EX: u16 = 150;
+pub const MSG_ANALOG2: u16 = 0x2002;
+pub const MSG_INAV_STATUS: u16 = 0x2000;
+pub const MSG_MISC2: u16 = 0x203a;
+
+#[derive(Debug, Clone)]
+pub enum MSPRes {
+    Ok,
+    Crc,
+    Dirn,
+    Fail,
+}
+
+impl Default for MSPRes {
+    fn default() -> Self {
+        MSPRes::Fail
+    }
+}
+
+#[derive(Debug, Default, Clone)]
+pub struct MSPMsg {
+    pub len: u16,
+    pub cmd: u16,
+    pub ok: MSPRes,
+    pub data: Vec<u8>,
+}
+
+fn crc8_dvb_s2(mut c: u8, a: u8) -> u8 {
+    c ^= a;
+    for _ in 0..8 {
+        if (c & 0x80) != 0 {
+            c = (c << 1) ^ 0xd5
+        } else {
+            c <<= 1
+        }
+    }
+    c
+}
+
+pub fn encode_msp2(cmd: u16, payload: &[u8]) -> Vec<u8> {
+    let paylen = payload.len();
+    let mut v = vec![0; paylen + 9];
+    v[0] = b'$';
+    v[1] = b'X';
+    v[2] = b'<';
+    v[3] = 0;
+    v[4] = (cmd & 0xff) as u8;
+    v[5] = (cmd >> 8) as u8;
+    v[6] = (paylen & 0xff) as u8;
+    v[7] = (paylen >> 8) as u8;
+    v[8..paylen + 8].copy_from_slice(payload);
+    let mut crc: u8 = 0;
+    for e in v.iter().take(paylen + 8).skip(3) {
+        crc = crc8_dvb_s2(crc, *e);
+    }
+    v[paylen + 8] = crc;
+    v
+}
+
+pub fn encode_msp(cmd: u16, payload: &[u8]) -> Vec<u8> {
+    let paylen = payload.len();
+    //    let mut v: Vec<u8> = Vec::new();
+    let mut v = vec![0; paylen + 6];
+    v[0] = b'$';
+    v[1] = b'M';
+    v[2] = b'<';
+    v[3] = paylen as u8;
+    v[4] = cmd as u8;
+    v[5..paylen + 5].copy_from_slice(payload);
+    let mut crc: u8 = v[3] ^ v[4];
+    for c in payload {
+        crc ^= c;
+    }
+    v[paylen + 5] = crc;
+    v
+}
+
+pub enum States {
+    Init,
+    M,
+    Dirn,
+    Len,
+    Cmd,
+    Data,
+    Crc,
+
+    XHeader2,
+    XFlags,
+    XId1,
+    XId2,
+    XLen1,
+    XLen2,
+    XData,
+    XChecksum,
+}
+
+pub fn reader<T>(mut sd: T, tx: crossbeam::channel::Sender<MSPMsg>)
+where
+    T: std::io::Read + Unpin,
+{
+    let mut msg = MSPMsg::default();
+    let mut n = States::Init;
+    let mut crc = 0u8;
+    let mut count = 0u16;
+    let mut dirnok = false;
+    let mut inp = [0u8; 256];
+    loop {
+        match sd.read(&mut inp) {
+            Ok(nbytes) => {
+                if nbytes == 0 {
+                    tx.send(MSPMsg::default()).unwrap();
+                    return;
+                } else {
+                    for e in inp.iter().take(nbytes) {
+                        match n {
+                            States::Init => {
+                                if *e == b'$' {
+                                    n = States::M;
+                                    msg.ok = MSPRes::Fail;
+                                    msg.len = 0;
+                                    msg.cmd = 0;
+                                    dirnok = false;
+                                }
+                            }
+                            States::M => {
+                                n = match *e {
+                                    b'M' => States::Dirn,
+                                    b'X' => States::XHeader2,
+                                    _ => States::Init,
+                                }
+                            }
+                            States::Dirn => match *e {
+                                b'!' => {
+                                    n = States::Len;
+                                }
+                                b'>' => {
+                                    n = States::Len;
+                                    dirnok = true;
+                                }
+                                _ => n = States::Init,
+                            },
+                            States::XHeader2 => match *e {
+                                b'!' => n = States::XFlags,
+                                b'>' => {
+                                    n = States::XFlags;
+                                    dirnok = true;
+                                }
+                                _ => n = States::Init,
+                            },
+                            States::XFlags => {
+                                crc = crc8_dvb_s2(0, *e);
+                                n = States::XId1;
+                            }
+                            States::XId1 => {
+                                crc = crc8_dvb_s2(crc, *e);
+                                msg.cmd = *e as u16;
+                                n = States::XId2;
+                            }
+                            States::XId2 => {
+                                crc = crc8_dvb_s2(crc, *e);
+                                msg.cmd |= (*e as u16) << 8;
+                                n = States::XLen1;
+                            }
+                            States::XLen1 => {
+                                crc = crc8_dvb_s2(crc, *e);
+                                msg.len = *e as u16;
+                                n = States::XLen2;
+                            }
+                            States::XLen2 => {
+                                crc = crc8_dvb_s2(crc, *e);
+                                msg.len |= (*e as u16) << 8;
+                                if msg.len > 0 {
+                                    n = States::XData;
+                                    count = 0;
+                                    msg.data = vec![0; msg.len.into()];
+                                } else {
+                                    n = States::XChecksum;
+                                }
+                            }
+                            States::XData => {
+                                crc = crc8_dvb_s2(crc, *e);
+                                msg.data[count as usize] = *e;
+                                count += 1;
+                                if count == msg.len {
+                                    n = States::XChecksum;
+                                }
+                            }
+                            States::XChecksum => {
+                                if crc != *e {
+                                    msg.ok = MSPRes::Crc
+                                } else {
+                                    msg.ok = if dirnok { MSPRes::Ok } else { MSPRes::Dirn };
+                                }
+                                tx.send(msg.clone()).unwrap();
+                                n = States::Init;
+                            }
+                            States::Len => {
+                                msg.len = *e as u16;
+                                crc = *e;
+                                n = States::Cmd;
+                            }
+                            States::Cmd => {
+                                msg.cmd = *e as u16;
+                                crc ^= *e;
+                                if msg.len == 0 {
+                                    n = States::Crc;
+                                } else {
+                                    msg.data = vec![0; msg.len.into()];
+                                    n = States::Data;
+                                    count = 0;
+                                }
+                            }
+                            States::Data => {
+                                msg.data[count as usize] = *e;
+                                crc ^= *e;
+                                count += 1;
+                                if count == msg.len {
+                                    n = States::Crc;
+                                }
+                            }
+                            States::Crc => {
+                                if crc != *e {
+                                    msg.ok = MSPRes::Crc;
+                                } else {
+                                    msg.ok = if dirnok { MSPRes::Ok } else { MSPRes::Dirn };
+                                }
+                                tx.send(msg.clone()).unwrap();
+                                n = States::Init;
+                            }
+                        }
+                    }
+                }
+            }
+            Err(_) => {
+                tx.send(MSPMsg::default()).unwrap();
+                return;
+            }
+        }
+    }
+}
